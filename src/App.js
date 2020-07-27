2f63af32b6fe61de8b04bc5cdea752c0787f663f
Anna Riazantseva <annarykim@gmail.com>
2020-07-27 18:39:29 +0300

    Added animated switch

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import Contacts from "./components/pages/Contacts";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PageFooter from "./components/layout/Footer";
 import ScrollTop from "./components/ScrollTop";
+import { AnimatedSwitch } from "react-router-transition";
 
 function App() {
   return (
     <Router>
       <div className="grey lighten-5 page-wrapper">
-        <Header />
         <ScrollTop />
-        <Switch>
+        <Header />
+        <AnimatedSwitch
+          atEnter={{ opacity: 0, transitionDuration: 1000 }}
+          atLeave={{ opacity: 0, transitionDuration: 1000 }}
+          atActive={{ opacity: 1, transitionDuration: 1000 }}
+          className="switch-wrapper"
+        >
           <Route exact path="/" component={Home} />
           <Route exact path="/menu" component={OurMenu} />
           <Route exact path="/services" component={Services} />
           <Route exact path="/contacts" component={Contacts} />
-        </Switch>
+        </AnimatedSwitch>
         <PageFooter />
       </div>
     </Router>
